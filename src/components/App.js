4a691ba8a381dce9bb55e672588e2860d7cf44ed
hadil <hihijazi@gmail.com>
2024-01-09 03:10:38 -0500

    save changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+const App = () => {
+  return (
+    <div>
+      <div className="header">
+        My Money
+      </div>
+      <div className="expense-tracker">
+        {/* Your expense tracker content goes here */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
 
